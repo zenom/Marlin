fcfc72037d07a50998b98ddd4e3fa7771f8510d2
Andrew Holman <andy@conspyre.com>
2020-01-24 15:48:43 -0500

    Adjust steps after changes.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -724,8 +724,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 426.37 }
-
+// #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 426.37 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 399.04 }
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
